fa7f3b5fe2041bcce6145ae686255e250953dd09
rj_m <rjmarte@sbcglobal.net>
2025-06-05 13:11:59 -0700

    Basic list generating JS

--- js/script.js
+++ js/script.js
@@ -16,5 +16,29 @@ var groceries = [
   "bananas",
   "coffee",
   "olive oil",
-  "mustard"
+  "mustard",
 ];
+
+//var horror = ["Oddities","Scream", "Cabin in the Woods", "Malignant", "Scream", "Tremors", "Silent Night", "Black Christmas", "Tremors"];
+
+//console.log(groceries.length);
+//18
+
+var deleteDuplicates = function (list) {
+  var cleanList = [];
+  for (var item of list) {
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    }
+  }
+  return cleanList;
+};
+
+var newGroceries = deleteDuplicates(groceries);
+
+//console.log(newGroceries.length);
+//15
+
+newGroceries.forEach(function (food, index) {
+  console.log(`#${index + 1} ${food}`);
+});
